aebcb47e9b65cef66271776f0f404a36d6a74f95
Meghavath Anitha <meghavathanitha9182@gmail.com>
2026-02-21 11:31:39 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+class Student{
+	int id;
+	String name;
+	double marks;
+	static string college="Jntu hyderabad";
+	public void details(){
+	System.out.println("ID:"+ id);
+	System.out.println("Name:"+ name);
+	System.out.println("Marks:"+ marks);
+	System.out.println("College:"+college);
+	}
+}
+	
